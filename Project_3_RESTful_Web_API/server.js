7b002b4e3b575e574fb7d5bdd761f1258b1ed13e
evripidispar <evripar@umd.edu>
2018-12-13 13:15:39 -0500

    Fix typos

--- Project_3_RESTful_Web_API/server.js
+++ Project_3_RESTful_Web_API/server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => res.status(404).send({error: 'Accepted endpoints: POS
  		res.json(block)
  	}
  	catch (error) {
- 		res.status(404).json({ error: 'Block could not be found in the blockchain'})
+ 		res.status(404).json({error: 'Block could not be found in the blockchain'})
  	}
  })
 
@@ -31,7 +31,7 @@ app.get('/', (req, res) => res.status(404).send({error: 'Accepted endpoints: POS
  app.post('/block', async (req, res)=> {
  	const body = req.body.body
  	if (body === '') {
- 		return res.status(400).json({ error: 'cannot create block with empty body string'})
+ 		return res.status(400).json({error: 'cannot create block with empty body string'})
  	}
  	else if (body === undefined){
  		return res.status(400).json({error: 'body parameter is not defined'})
